3a69ce4c3382bb18f18c696de196fab5f2fd46a7
vovap <vovaprimachuk@gmail.com>
2021-07-04 16:01:31 +0300

    change the look and functionality admin site. connect media

--- realtors/models.py
+++ realtors/models.py
@@ -6,7 +6,7 @@ class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/')
     desctiption = models.TextField(blank=True)
-    photo = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, default='555-555-5555')
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
