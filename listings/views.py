6fc40ed0ef88dc9e96d2b96f0ab00b8a2e0ece5a
vovap <vovaprimachuk@gmail.com>
2021-07-05 22:41:51 +0300

    pagination. single listing page. search form choices. search form filter.

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,85 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import *
 
 from .models import Listings
 
 
 def index(request):
-    listings = Listings.objects.all()
-    context = {'listings': listings}
+    listings = Listings.objects.order_by(
+        '-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings': paged_listings}
+
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listings, pk=listing_id)
+
+    # getting internal photos
+    internal_photos = []
+    for i in range(1, 6):
+        if getattr(listing, 'photo_%d' % i):
+            photo = getattr(listing, 'photo_%d' % i)
+            internal_photos.append(photo)
+
+    context = {
+        'listing': listing,
+        'internal_photos': internal_photos
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listings.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
+    context = {
+        'listings': queryset_list,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'values': request.GET
+    }
+
+    return render(request, 'listings/search.html', context)
