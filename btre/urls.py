4eb6c0584a92d7046d2448197dd0129ea9608066
vovap <vovaprimachuk@gmail.com>
2021-07-07 22:36:40 +0300

    account and authentification

--- btre/urls.py
+++ btre/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -20,8 +21,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-  path('', include('pages.urls')),
-  path('listings/', include('listings.urls')),
-  path('realtors/', include('realtors.urls')),
-  path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('listings/', include('listings.urls')),
+    path('realtors/', include('realtors.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
